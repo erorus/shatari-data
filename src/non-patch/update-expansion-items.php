a4c375f5d3a3b2ae45414a6d84293e09eb7ef696
Erorus <erorus@users.noreply.github.com>
2026-01-20 23:04:52 -0500

    update tww expansion items/pets

--- src/non-patch/update-expansion-items.php
+++ src/non-patch/update-expansion-items.php
@@ -14,7 +14,7 @@ fclose($f);
 
 $json = json_decode(file_get_contents($argv[2]), true);
 foreach ($ids as $id) {
-    $json[$id] ??= 10;
+    $json[$id] ??= 11;
 }
 ksort($json, SORT_NUMERIC);
 file_put_contents($argv[2], json_encode($json, JSON_PRETTY_PRINT));
