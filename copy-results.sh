5cd8dc4c003629225d8e2a2f779b7c40f652fc78
Erorus <erorus@users.noreply.github.com>
2026-03-19 00:40:41 -0400

    update paths for new module-based shatari-front

--- copy-results.sh
+++ copy-results.sh
@@ -3,4 +3,4 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 cp -v out/battlepets.json out/bonuses.json out/items.all.json out/names.bound.*.json ../shatari/
-cp -v out/battlepets.json out/battlepets.*.json out/categories.*.json out/items.unbound.json out/names.unbound.*.json out/name-suffixes.*.json out/vendor.json out/bonusToStats.json ../shatari-front/public/json/
+cp -v out/battlepets.json out/battlepets.*.json out/categories.*.json out/items.unbound.json out/names.unbound.*.json out/name-suffixes.*.json out/vendor.json out/bonusToStats.json ../shatari-front/json/
